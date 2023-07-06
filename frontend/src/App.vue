3c922bc1c20132f1edbd150f4af5d8c7e512ca80
s481809 <wlakuc@st.amu.edu.pl>
2023-07-07 00:05:07 +0200

    * Setup vue-router * Create BaseContainer and TheHeader components * Create Home view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,13 @@
 <template>
-  <h1>Main page</h1>
+  <TheHeader class="mb-10"></TheHeader>
+  <main>
+    <BaseContainer>
+      <RouterView></RouterView>
+    </BaseContainer>
+  </main>
 </template>
+
+<script setup lang="ts">
+import TheHeader from "@/components/layouts/TheHeader.vue";
+import BaseContainer from "@/components/base/BaseContainer.vue";
+</script>
---
+++ frontend/src/components/layouts/TheHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="bg-zinc-900 py-3 text-white">
+    <nav>
+      <BaseContainer>
+        <RouterLink to="/" class="text-xl font-bold">Go Court</RouterLink>
+      </BaseContainer>
+    </nav>
+  </header>
+</template>
+
+<script setup lang="ts">
+import BaseContainer from "@/components/base/BaseContainer.vue";
+</script>
